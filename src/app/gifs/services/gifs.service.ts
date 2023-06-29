97c0f88c2668fad1dd4c00c6acdc446602671781
Oriana Romero <oriana.nahir@bosonit.com>
2023-06-29 13:36:29 +0200

    gifsapp done

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,12 +12,16 @@ export class GifsService {
   private apiKey: string = 'apbgYC1hByQF5hQEglstXhruoOt1Fp8D';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
+  //Getter for history
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
+  //Organize search history
   private organizedHistory(tag: string) {
     tag = tag.toLocaleLowerCase();
 
@@ -28,24 +32,41 @@ export class GifsService {
     this._tagsHistory.unshift(tag);
 
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
+  //Save history in local storage
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  //Load local storage info
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) {
+      return;
+    }
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0) {
+      return;
+    }
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  //Search function
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizedHistory(tag);
 
     const params = new HttpParams()
-    .set('api_key', this.apiKey)
-    .set('limit', 10)
-    .set('q', tag)
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
 
     this.http
-      .get<SearcResponse>(
-        `${this.serviceUrl}/search`, {params}
-      )
-      .subscribe(res => {
+      .get<SearcResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((res) => {
         this.gifsList = res.data;
-        console.log(this.gifsList);
       });
 
     // fetch('https://api.giphy.com/v1/gifs/search?api_key=apbgYC1hByQF5hQEglstXhruoOt1Fp8D&q=valorant&limit=10')
